25ac1de732876c6a20c97c3c28896482cfa0ce09
Fanel Secara <fanelsecara@gmail.com>
2021-09-07 16:46:02 +0200

    Final project

---
+++ index.js
@@ -0,0 +1,33 @@
+const randomColor = require('randomcolor');
+const chalk = require('chalk');
+
+const hueColor = process.argv[2];
+const luminosityColor = process.argv[3];
+
+
+const selectedColor = randomColor({
+  hue: hueColor,
+  luminosity: luminosityColor,
+});
+
+if (process.argv[2] === 'ask') {
+  console.log('gimme color and luminosity');
+} else {
+  const amountColumn = 31;
+  const amountRow = 9;
+  let square = '';
+  for (let row = 1; row <= amountRow; row++) {
+    for (let column = 1; column <= amountColumn; column++) {
+      if (column === 14 && row === 5) {
+        square += selectedColor;
+        column += 6;
+      } else if (column > 3 && column < 29 && row > 3 && row < 7) {
+        square += ' ';
+      } else {
+        square += '#';
+      }
+    }
+    square += '\n';
+  }
+  console.log(chalk.hex(selectedColor)(square.replace(/\n+$/, '')));
+}
